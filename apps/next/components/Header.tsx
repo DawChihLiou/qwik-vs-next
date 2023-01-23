8140a79d146f3127bd63379aa58a4cb2a6a00cd1
Daw-Chih Liou <dawochih.liou@gmail.com>
2023-01-23 12:45:31 +0100

    refactor: header

--- apps/next/components/Header.tsx
+++ apps/next/components/Header.tsx
@@ -2,13 +2,25 @@ import { NextLogo } from "./icons/next";
 
 export default function Header() {
   return (
-    <header className="container mx-auto px-4 py-8">
-      <div className="flex gap-2">
-        <a href="/" title="demo">
+    <header className="navbar bg-base-100 py-8 px-0">
+      <div className="flex-1">
+        <a href="/" className="btn btn-ghost normal-case text-xl">
           <NextLogo />
         </a>
       </div>
-      <ul></ul>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a
+              href="https://github.com/DawChihLiou/qwik-vs-next"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
--- apps/qwik/src/components/header/header.tsx
+++ apps/qwik/src/components/header/header.tsx
@@ -3,13 +3,25 @@ import { QwikLogo } from "../icons/qwik";
 
 export default component$(() => {
   return (
-    <header class="container mx-auto px-4 py-8">
-      <div class="flex gap-2">
-        <a href="/" title="demo">
+    <header class="navbar bg-base-100 py-8 px-0">
+      <div class="flex-1">
+        <a href="/" class="btn btn-ghost normal-case text-xl">
           <QwikLogo />
         </a>
       </div>
-      <ul></ul>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li>
+            <a
+              href="https://github.com/DawChihLiou/qwik-vs-next"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 });
