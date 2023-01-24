20213f8a16f3b3b9ad703eaefd5fa7c9b6dde40c
Daw-Chih Liou <dawochih.liou@gmail.com>
2023-01-24 14:04:47 +0100

    refactor: remove a

--- apps/next/components/Header.tsx
+++ apps/next/components/Header.tsx
@@ -1,24 +1,25 @@
+import Link from "next/link";
 import { NextLogo } from "./icons/next";
 
 export default function Header() {
   return (
     <header className="navbar bg-base-100 py-8 px-0">
       <div className="flex-1">
-        <a href="/" className="btn btn-ghost normal-case text-xl">
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
           <NextLogo />
-        </a>
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a
+            <Link
               href="https://github.com/DawChihLiou/qwik-vs-next"
               target="_blank"
               rel="noreferrer"
             >
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src="/github.svg" alt="Link to repository" className="w-8" />
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
